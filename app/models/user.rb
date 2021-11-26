d93ef5f7d5c0786246f8173ba0d4f07240bc9f2f
koray-eren <koray.eren@outlook.com>
2021-11-26 15:07:46 +1100

    amended user:order relationship to has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :listings
-  has_one :order
+  has_many :orders
 
   validates :suburb, :state, presence: true
 
